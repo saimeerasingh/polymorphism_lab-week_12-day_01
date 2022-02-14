13591bc9b15207ac42a30aa43a4a8390e485b842
saimeerasingh <singhsaimeera@gmail.com>
2022-02-14 15:08:18 +0000

    Card class extended to credit card

--- src/main/java/Card.java
+++ src/main/java/Card.java
@@ -1,3 +1,23 @@
 public abstract class Card {
-    
+    private String cardNumber;
+    private String expiryDate;
+    private int securityNumber;
+
+    public Card(String cardNumber, String expiryDate, int securityNumber) {
+        this.cardNumber = cardNumber;
+        this.expiryDate = expiryDate;
+        this.securityNumber = securityNumber;
+    }
+
+    public String getCardNumber() {
+        return cardNumber;
+    }
+
+    public String getExpiryDate() {
+        return expiryDate;
+    }
+
+    public int getSecurityNumber() {
+        return securityNumber;
+    }
 }
