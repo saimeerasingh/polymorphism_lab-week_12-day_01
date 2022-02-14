dd1f3bf31e6cdeaf07a2cfd361140c9472ccdab2
saimeerasingh <singhsaimeera@gmail.com>
2022-02-14 15:38:59 +0000

    cash added to wallet

--- src/main/java/Wallet.java
+++ src/main/java/Wallet.java
@@ -3,11 +3,13 @@ import java.util.ArrayList;
 public class Wallet {
     private String name;
     private ArrayList<IScan> cards;
+    private ArrayList<Cash> monies;
 
 
     public Wallet(String name) {
         this.name = name;
         this.cards = new ArrayList<IScan>();
+        this.monies = new ArrayList<Cash>();
     }
 
     public String getName() {
@@ -15,10 +17,14 @@ public class Wallet {
     }
 
     public int getNumberOfItems() {
-        return this.cards.size();
+        return this.cards.size() + this.monies.size();
     }
 
     public void addItem(IScan card) {
         this.cards.add(card);
+    }
+
+    public void addItem(Cash cash){
+        this.monies.add(cash);
     }
 }
